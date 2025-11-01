a837ed4361632c1033287bda71b6e5b98ae8ca41
OliverScrubble <luigiXVImycousin@outlook.com>
2025-11-01 15:52:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 # Configurazione
 MATRIX_WEBHOOK = "https://matrix.org/_matrix/client/r0/rooms/!skCsZdyGNtJBgEQDDL:matrix.org/send/m.room.message"
-MATRIX_TOKEN = "mat_PlzYZMyyjtJ6bLhSWhjwFseS0245tZ_0d9P8q"
+MATRIX_TOKEN = "mat_ODGV8OXrsTxufmSAhqgaMNGdyxAejW_eFbnI3"
 
 class ClientManager:
     def __init__(self):
